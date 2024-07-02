7ae23603db21603f27a7135cdf72163fd92e4bc2
chaithra1607 <chaismile88@gmail.com>
2024-07-02 11:48:59 +0530

    done

---
+++ python.py
@@ -0,0 +1,62 @@
+#balance amount
+'''money=int(input())
+if money>100:
+    print("balance amount")
+else:
+    print("not balance amount")'''
+#even and odd numbers
+'''marks=int(input())
+if marks%2==0:
+    print("even")
+else:
+    print("odd")'''
+#grading 
+'''grade=int(input())
+if grade>=90:
+    print("excellent")
+elif grade>80 & grade<90 :
+    print("good") 
+marks=int(input())
+if marks>=90:
+    print("excellent")
+elif marks>=85 and marks<90:
+    print("very good")
+elif marks>=70 and marks<85:
+    print("good")
+elif marks>=55 and marks<70:
+    print("average")
+elif marks>=35 and marks<55:
+    print("poor")
+else:
+    print("fail")'''
+#positive numbers
+'''number=int(input())
+if number>=1:
+    print("positive")
+elif number<0:
+    print("negative")
+else:
+    print("it is zero")'''
+#staring 
+'''sales=int(input())
+if sales>=100:
+    print(" 5 stars")
+elif sales>=50 and sales<=99:
+    print("3.5 stars")
+elif sales>=20 and sales<50:
+    print("2.5 stars")
+else:
+    print("1 stars")'''
+'''stars=int(input())
+if stars>=1:
+    print("positive stars")
+elif stars<0:
+    print("negative stars")
+else:
+    print("zero stars")'''
+
+
+
+    
+    
+    
